fbaef4d4ddc454274844ce6f9cfbbe38db5f3482
slimane benamar <slimanebnr@gmail.com>
2021-02-22 00:51:48 +0100

    import Carousel, insertion de deux extemple teach'r

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+export default class App extends React.Component {
+  constructor(props){
+          super(props);
+          this.state = {
+            activeIndex:0,
+            carouselItems: [
+            {
+                nom:"Teach'r 1",
+                formation: "univ",
+                description: "Description 1",
+            },
+            {
+                nom:"Teach'r 2",
+                formation: "univ",
+                description: "Description 2",
+            },
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+          ]
+        }
+      }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      _renderItem({item,index}){
+          return (
+            <View style={{
+                backgroundColor:'#eaeaea',
+                borderwidth: 4,
+                borderRadius: 8,
+                height: 450,
+                padding: 50,
+                marginLeft: 25,
+                //marginRight: 25,
+               }}>
+              <Text style={{fontSize: 30}}>{item.nom}</Text>
+              <Text>{item.formation}</Text>
+              <Text>{item.description}</Text>
+            </View>
+
+          )
+      }
+
+      render() {
+          return (
+            <SafeAreaView style={{flex: 1, paddingTop: 50, }}>
+              <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center', alignItems:'center' }}>
+                  <Carousel
+                    layout={"tinder"}
+                    ref={ref => this.carousel = ref}
+                    data={this.state.carouselItems}
+                    sliderWidth={300}
+                    itemWidth={300}
+                    renderItem={this._renderItem}
+                    onSnapToItem = { index => this.setState({activeIndex:index}) } />
+              </View>
+            </SafeAreaView>
+          );
+      }
+  }
